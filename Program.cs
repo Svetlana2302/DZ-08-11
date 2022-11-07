69898b58b69c1e94575e816f8c441e6124c1937c
Svetlana <korneevdenis.ru@mail.ru>
2022-11-07 17:48:17 +0300

    задание один

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите номер дня недели");
+int number = int.Parse(Console.ReadLine()!);
+
+//int dayoff(int number)
+{
+    int day = 0;
+    if(day >= 1 && day <= 5)
+    {
+        Console.WriteLine("нет");
+    }
+    else Console.WriteLine("да");
+}
+
+//int  res = dayoff(number);
+//Console.WriteLine(res);
+
+
